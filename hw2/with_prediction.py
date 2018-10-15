f7e72032aa91709eb11703df957fc7b83bb9bd95
seiz <seiz@sfu.ca>
2018-10-14 18:26:45 -0700

    Remove global variables and fix score calculating bug

--- hw2/with_prediction.py
+++ hw2/with_prediction.py
@@ -14,25 +14,6 @@ from copy import deepcopy
 from datetime import datetime
 import pdb
 
-pp = pprint.PrettyPrinter(width=45, compact=True)
-
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-f', '--file', default='data/cipher.txt', help='cipher file')
-arg_parser.add_argument('-b', '--beamsize', type=int, default=1000)
-arg_parser.add_argument('--cuda', action='store_true', default=False)
-args = arg_parser.parse_args()
-
-lm_order = 6
-contiguous_score_weights = [0,0,1,1,1,2,3]
-
-ext_limits = 7
-
-print('Loading language model')
-lm = LM("data/6-gram-wiki-char.lm.bz2", n=lm_order, verbose=False)
-model = nlm.load_model("data/mlstm_ns.pt", cuda=args.cuda)
-print('Language model loaded')
-
-
 def read_file(filename):
     if filename[-4:] == ".bz2":
         with bz2.open(filename, 'rt') as f:
@@ -58,13 +39,12 @@ def check_limits(mappings, ext_limits, letter_to_check=None):
         return plaintext_letters.count(letter_to_check) <= ext_limits
 
 
-def score(Ve, mappings, cipher_text, cipher_letter, old_score, cuda):
+def score(Ve, mappings, cipher_text, cipher_letter, old_score, lm_order, lm, model, cuda):
     # Replace with existing mappings
     deciphered = [mappings[cipher_letter] if cipher_letter in mappings else '_' for cipher_letter in cipher_text]
-    percentage_unknown = Counter(deciphered)['_'] / len(deciphered)
     score = 0
 
-    if percentage_unknown < 0.9:
+    if len(mappings) > 1:
         seq = ''
         score = old_score
         length = cipher_text.rfind(cipher_letter) + 1
@@ -72,10 +52,11 @@ def score(Ve, mappings, cipher_text, cipher_letter, old_score, cuda):
             c = deciphered[i]
             if cipher_text[i] == cipher_letter:
                 # Calculate score for the newly ciphered letters using nlm
+                # Note that nlm scores are positive (-log(Prob)).
                 if i == 0:
-                    score += nlm.score_first(c, model, cuda)
+                    score -= nlm.score_first(c, model, cuda)
                 else:
-                    score += nlm.score_next(c, seq, model, cuda)
+                    score -= nlm.score_next(c, seq, model, cuda)
             elif c == '_':
                 # Predict unknown letters
                 if i == 0:
@@ -84,7 +65,7 @@ def score(Ve, mappings, cipher_text, cipher_letter, old_score, cuda):
                 else:
                     llh_predictions = nlm.next_chars(seq[:i], cuda, model, k=5, cutoff='symbol')
                     if llh_predictions:
-                        # Last element has the highest prob
+                        # Last element has the lowest nlm score and highest prob
                         c = llh_predictions[-1][0]
                     else:
                         # nlm gives no prediction. Use ngram instead.
@@ -116,12 +97,11 @@ def prune(beams, beamsize):
     return sorted_beams[:beamsize]
 
 
-def beam_search(cipher_text, ext_order, ext_limits, init_beamsize, cuda):
+def beam_search(cipher_text, ext_order, Ve, ext_limits, init_beamsize, lm_order, lm, model, cuda):
     Hs = []
     Ht = []
     cardinality = 0
     Hs.append(({}, 0))
-    Ve = string.ascii_lowercase
 
     while cardinality < len(ext_order):
         beamsize = int(init_beamsize*(0.94**cardinality))
@@ -134,7 +114,8 @@ def beam_search(cipher_text, ext_order, ext_limits, init_beamsize, cuda):
                 ext_mappings = deepcopy(mappings)
                 ext_mappings[cipher_letter] = plain_letter
                 if check_limits(ext_mappings, ext_limits, plain_letter):  # only check new added one
-                    Ht.append((ext_mappings, score(Ve, ext_mappings, cipher_text, cipher_letter, sc, cuda)))
+                    Ht.append((ext_mappings, score(Ve, ext_mappings, cipher_text, cipher_letter, sc, 
+                                                   lm_order, lm, model, cuda)))
         Hs = prune(Ht, beamsize)
         cardinality += 1
         Ht = []
@@ -142,7 +123,7 @@ def beam_search(cipher_text, ext_order, ext_limits, init_beamsize, cuda):
     return Hs[0]
 
 
-def contiguous_score(cipher, order):
+def contiguous_score(cipher, order, lm_order, weights):
     order = set(order)
     count = 0
     ngrams = defaultdict(int)
@@ -157,7 +138,7 @@ def contiguous_score(cipher, order):
             count = 0
     score = 0
     for k, v in ngrams.items():
-        score += contiguous_score_weights[k] * v
+        score += weights[k] * v
     return score
 
 
@@ -167,7 +148,7 @@ def prune_orders(orders, beamsize):
     return sorted_order[: beamsize]
 
 
-def search_ext_order(cipher, beamsize):
+def search_ext_order(cipher, beamsize, lm_order, contiguous_score_weights):
     symbols = set(cipher)
     # Start with the most common character
     freq = Counter(cipher)
@@ -182,7 +163,7 @@ def search_ext_order(cipher, beamsize):
                     new_order = deepcopy(order)
                     new_order.append(symbol)
                     new_scores = deepcopy(scores)
-                    new_scores.insert(0, contiguous_score(cipher, new_order))
+                    new_scores.insert(0, contiguous_score(cipher, new_order, lm_order, contiguous_score_weights))
                     orders_tmp.append((new_scores, new_order))
         orders = prune_orders(orders_tmp, beamsize)
         orders_tmp = []
@@ -192,18 +173,38 @@ def search_ext_order(cipher, beamsize):
 
 if __name__ == '__main__':
 
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-f', '--file', default='data/cipher.txt', help='cipher file')
+    arg_parser.add_argument('-b', '--beamsize', type=int, default=1000)
+    arg_parser.add_argument('--cuda', action='store_true', default=False)
+    args = arg_parser.parse_args()
+
+    lm_order = 6
+    contiguous_score_weights = [0,0,1,1,1,2,3]
+
+    ext_limits = 7
+
+    Ve = string.ascii_lowercase
+
+    print('Loading language model')
+    lm = LM("data/6-gram-wiki-char.lm.bz2", n=lm_order, verbose=False)
+    model = nlm.load_model("data/mlstm_ns.pt", cuda=args.cuda)
+    print('Language model loaded')
+
     cipher = read_file(args.file)
     cipher = [x for x in cipher if not x.isspace()]
     cipher = ''.join(cipher)
 
-    ext_order = search_ext_order(cipher, 100)
+    ext_order = search_ext_order(cipher, 100, lm_order, contiguous_score_weights)
     print(ext_order)
 
     print('Start deciphering...')
     search_start = datetime.now()
-    mappings, sc = beam_search(cipher, ext_order, ext_limits, args.beamsize, args.cuda)
+    mappings, sc = beam_search(cipher, ext_order, Ve, ext_limits, args.beamsize, 
+                               lm_order, lm, model, args.cuda)
     search_end = datetime.now()
     print('Deciphering completed after {}'.format(search_end - search_start))
+
     print(mappings)
     deciphered = [mappings[c] if c in mappings else '_' for c in cipher]
     deciphered = ''.join(deciphered)
