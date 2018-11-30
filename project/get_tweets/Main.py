15c9fbed5d41fb7621b9c9a31d69fc72f951db84
seiz <seiz@sfu.ca>
2018-11-29 23:41:59 -0800

    Keep tweets that really at the celebrity. Sort tweets by celebrities and dates.

--- project/get_tweets/Main.py
+++ project/get_tweets/Main.py
@@ -1,44 +1,83 @@
 import sys
 if sys.version_info[0] < 3:
-    import got
+  import got
 else:
-    import got3 as got
+  import got3 as got
+from datetime import date, timedelta
+from dateutil.rrule import rrule, DAILY
+import os
+import pdb
 
-def main():
-
-	def printTweet(descr, t):
-		print(descr)
-		print("Username: %s" % t.username)
-		print("Retweets: %d" % t.retweets)
-		print("Text: %s" % t.text)
-		print("Mentions: %s" % t.mentions)
-		print("Hashtags: %s\n" % t.hashtags)
-
-	# # Example 1 - Get tweets by username
-	# tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
-	# tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	# printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
-
-	# # Example 2 - Get tweets by query search
-	# tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
-	# tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	# printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
-
-	# # Example 3 - Get tweets by username and bound dates
-	# tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
-	# tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	# printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
-
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch("@realDonaldTrump").setSince("2018-10-01").setUntil("2018-10-07").setMaxTweets(5000)
-	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-	print("Tweets that mention Donald Trump from 2018-10-01 and 2018-10-07\n\n")
-	for tweet in tweets:
-	  print("@%s"%(tweet.username))
-	  print(tweet.text.encode('utf-8'))
-	  print('\n')
+def printTweet(descr, t):
+  print(descr)
+  print("Username: %s" % t.username)
+  print("Retweets: %d" % t.retweets)
+  print("Text: %s" % t.text)
+  print("Mentions: %s" % t.mentions)
+  print("Hashtags: %s\n" % t.hashtags)
 
 if __name__ == '__main__':
-	main()
+  # # Example 1 - Get tweets by username
+  # tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
+  # tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+  # printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
+
+  # # Example 2 - Get tweets by query search
+  # tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
+  # tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+  # printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
+
+  # # Example 3 - Get tweets by username and bound dates
+  # tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
+  # tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+  # printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
+
+  save_dir = 'output'
+  if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+  # Chosen according to https://fanpagelist.com/category/celebrities/view/list/sort/influence/
+  celebrity_usernames = ['barackobama', 'realdonaldtrump', 'britneyspears', 'thebeatles', 'nineinchnails',
+                         'elvispresley', 'jtimberlake', 'rihanna', 'cher', 'michaeljackson',
+                         'beyonce', 'kingjames', 'ladygaga', 'rogerfederer', 'liltunechi']
+  from_date = date(2018, 1, 1)
+  to_date = date(2018, 11, 28)
+  daily_fetch = 5000
+
+  # For testing
+  celebrity_usernames = ['barackobama']
+  from_date = date(2018, 11, 1)
+  to_date = date(2018, 11, 3)
+  daily_fetch = 100
+
+  for celebrity_username in celebrity_usernames:
+    keyword = "@%s"%(celebrity_username)
+
+    sub_save_dir = os.path.join(save_dir, celebrity_username)
+    if not os.path.exists(sub_save_dir):
+      os.makedirs(sub_save_dir)
+
+    for dt in rrule(freq=DAILY, dtstart=from_date, until=to_date):
+      dt_str = dt.strftime("%Y-%m-%d")
+      nxt_dt = dt + timedelta(days=1)
+      nxt_dt_str = nxt_dt.strftime("%Y-%m-%d")
+      tweetCriteria = got.manager.TweetCriteria() \
+                      .setQuerySearch(keyword) \
+                      .setSince(dt_str) \
+                      .setUntil(nxt_dt_str) \
+                      .setMaxTweets(daily_fetch)
+      tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+      f = open(os.path.join(sub_save_dir, dt_str), "w")
+      for tweet in tweets:
+        # pdb.set_trace()
+        try:
+          if keyword in tweet.text.lower():
+            print(tweet.username)
+            print(tweet.date)
+            print(tweet.text)
+            print('\n')
+            f.write("%s\n"%tweet.text)
+        except:
+          # Tweet might inlude non-ASCII characters
+          continue
+      f.close()
