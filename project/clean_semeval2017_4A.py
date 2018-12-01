af14f83319e4e765f544e19a278d5621f929ba7d
Leo Li <haipengl@sfu.ca>
2018-12-01 02:13:21 -0800

    update data

--- project/clean_semeval2017_4A.py
+++ project/clean_semeval2017_4A.py
@@ -8,7 +8,7 @@ import clean_data
 
 
 if __name__ == '__main__':
-    folder = os.path.join('data', 'datastories-semeval2017-task4')
+    folder = os.path.join('datastories-semeval2017-task4')
     path = os.path.join(folder, 'train.txt')
     df = pd.read_csv(path, encoding='latin1', usecols=[1, 2], header=None, names=['tag', 'tweet'],  delimiter='\t')
     df['cleaned_tweet'] = df.tweet.apply(clean_data.clean_tweet)
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     cleandf['tag'][cleandf['tag'] == 'negative'] = 0
     cleandf['tag'][cleandf['tag'] == 'neutral'] = 1
     cleandf['tag'][cleandf['tag'] == 'positive'] = 2
-    small_df, _ = train_test_split(cleandf, test_size=0.8)
+    # small_df, _ = train_test_split(cleandf, test_size=0.8)
+    small_df = cleandf
     small_train, small_test = train_test_split(small_df, test_size=0.1)
     small_train = small_train.reset_index(drop=True)
     small_test = small_test.reset_index(drop=True)
