a86dd8ee6f8cee6eedd556a9cdf3927bb5b79ef5
Zhaoyang Li <zhaoyang_li@sfu.ca>
2018-12-01 01:26:26 -0800

    add f1 score mesurement

--- project/train.py
+++ project/train.py
@@ -11,10 +11,18 @@ from util import convert_data_to_features, check_path
 from tqdm import tqdm
 import numpy as np
 import time, argparse
+from sklearn.metrics import confusion_matrix
 
-def accuracy(out, labels):
-    outputs = np.argmax(out, axis=1)
-    return np.sum(outputs == labels)
+def comp_scores(out, labels):
+    # return accuracy and f1 score
+    pred = np.argmax(out, axis=1)
+    correct = np.sum(pred == labels)
+    cm = confusion_matrix(labels, pred)
+    tn = cm[0][0]
+    fn = cm[1][0]
+    tp = cm[1][1]
+    fp = cm[0][1]
+    return correct, tp, tn, fp, fn
 
 def positive_score(out):
     outputs = F.softmax(out, dim=1)
@@ -23,7 +31,7 @@ def positive_score(out):
 
 
 class TwitterSentiment():
-    def __init__(self, train_file, test_file, batch_size=16, num_epoch=10, log_interval=100,
+    def __init__(self, train_file, test_file, batch_size=16, num_epoch=10, log_interval=100, max_seq_len=100,
                  prototype=False, parallel=False, load_model=None):
         self.log_interval = log_interval
         self.batch_size = batch_size
@@ -35,8 +43,8 @@ class TwitterSentiment():
         if prototype:
             self.train_data = self.train_data[:10000]
             self.test_data = self.test_data[:1000]
-        self.train_set = TweetsDataset(self.train_data, tokenizer, 100)
-        self.test_set = TweetsDataset(self.test_data, tokenizer, 100)
+        self.train_set = TweetsDataset(self.train_data, tokenizer, max_seq_len)
+        self.test_set = TweetsDataset(self.test_data, tokenizer, max_seq_len)
 
         # train_features = convert_data_to_features(self.train_data, [0, 1], 200, tokenizer)
         # train_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
@@ -102,9 +110,10 @@ class TwitterSentiment():
     def test(self):
         self.model.eval()
         eval_loss = 0
-        eval_accuracy = 0
+        num_correct = 0
         batches_count = 0
         data_count = 0
+        tp, tn, fp, fn = 0, 0, 0, 0
         with torch.no_grad():
             for i, batch in enumerate(tqdm(self.test_loader)):
                 batches_count += 1
@@ -115,12 +124,21 @@ class TwitterSentiment():
                 eval_loss += loss.item()
                 logits = logits.cpu().numpy()
                 label_ids = label_ids.cpu().numpy()
-                eval_accuracy += accuracy(logits, label_ids)
+                correct, tp_t, tn_t, fp_t, fn_t = comp_scores(logits, label_ids)
+                num_correct += correct
+                tp += tp_t
+                tn += tn_t
+                fp += fp_t
+                fn += fn_t
 
         eval_loss /= batches_count
-        eval_accuracy /= data_count
+        eval_accuracy = num_correct / data_count
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1 = 2 * precision * recall / (precision + recall)
 
-        print('evaluation loss: {:.4}, accuracy: {:.4}%'.format(eval_loss, eval_accuracy * 100))
+        print('evaluation loss: {:.4}, accuracy: {:.4}% f1 score: {:.4}'.format(
+            eval_loss, eval_accuracy * 100, f1))
 
     def save_model(self, file):
         torch.save(self.model.state_dict(), file)
@@ -139,9 +157,9 @@ if __name__ == '__main__':
 
     train_file = 'data/train.csv'
     test_file = 'data/test.csv'
-    save_file = 'saved_model/bert_tweet_big.m'
+    save_file = 'saved_model/bert_tweet_big.pth'
     check_path('saved_model')
-    ckpt_file = 'saved_model/bert_ckpt.m'
+    ckpt_file = 'saved_model/bert_ckpt.pth'
     log_interval = 100
     twitter_sentiment = TwitterSentiment(train_file, test_file, num_epoch=args.num_epoch, load_model=args.load_model,
                             batch_size=args.batchsize, log_interval=log_interval, prototype=args.pt, parallel=True)
